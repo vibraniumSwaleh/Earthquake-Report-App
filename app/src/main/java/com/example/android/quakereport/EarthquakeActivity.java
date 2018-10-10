770a6e38ac3a46a5cb6bd750fe84462f30210090
Swaleh Hussein <hus.swaleh@yahoo.com>
2018-10-10 03:07:55 +0300

    Preferences added and working

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,21 +15,22 @@
  */
 package com.example.android.quakereport;
 
-import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.support.v4.content.Loader;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -52,8 +53,7 @@ public class EarthquakeActivity extends AppCompatActivity implements android.sup
     /**
      * URL for earthquake data from the USGS dataset
      */
-    private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
     /**
      * Adapter for the list of earthquakes
@@ -128,23 +128,51 @@ public class EarthquakeActivity extends AppCompatActivity implements android.sup
 
     }
 
+    // onCreateLoader instantiates and returns a new Loader for the given ID
     @Override
     public android.support.v4.content.Loader<List<EarthquakeClass>> onCreateLoader(int i, Bundle bundle) {
         // TODO: Create a new loader for the given URL
         // Create a new loader for the given URL
-        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+            // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+            String minMagnitude = sharedPrefs.getString(
+                    getString(R.string.settings_min_magnitude_key),
+                    getString(R.string.settings_min_magnitude_default));
+
+            String orderBy = sharedPrefs.getString(
+                    getString(R.string.settings_order_by_key),
+                    getString(R.string.settings_order_by_default));
+
+            // parse breaks apart the URI string that's passed into its parameter
+            Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+
+            // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+            Uri.Builder uriBuilder = baseUri.buildUpon();
+
+            // Append query parameter and its value. For example, the `format=geojson`
+            uriBuilder.appendQueryParameter("format", "geojson");
+            uriBuilder.appendQueryParameter("limit", "10");
+            uriBuilder.appendQueryParameter("minmag", minMagnitude);
+            uriBuilder.appendQueryParameter("orderby", "time");
+            uriBuilder.appendQueryParameter("orderby", orderBy);
+
+            Log.v(LOG_TAG,"Query is working");
+            // Return the completed uri `http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=10&minmag=minMagnitude&orderby=time
+            return new EarthquakeLoader(this, uriBuilder.toString());
+
     }
 
     @Override
     public void onLoadFinished(Loader<List<EarthquakeClass>> loader, List<EarthquakeClass> earthquakes) {
 
         // TODO: Update the UI with the result
-        // Set empty state text to display "No earthquakes found."
-        mEmptyStateTextView.setText(R.string.no_internet_connection);
         // Clear the adapter of previous earthquake data
         // Hide loading indicator because the data has been loaded
         View loadingIndicator = findViewById(R.id.loading_spinner);
         loadingIndicator.setVisibility(View.GONE);
+
         aDapter.clear();
         // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
         // data set. This will trigger the ListView to update.
@@ -177,7 +205,7 @@ public class EarthquakeActivity extends AppCompatActivity implements android.sup
         }
         return super.onOptionsItemSelected(item);
     }
-    
+
 }
 
 
